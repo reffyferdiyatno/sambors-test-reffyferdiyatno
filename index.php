5a6edb908b3f7cff488ff1184d1bda2bbd2ef56b
reffyferdiyatno <62872628+reffyferdiyatno@users.noreply.github.com>
2020-07-03 14:17:40 +0700

    Update and rename indext.php to index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Pendaftaran</title>
+	<title>data</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<div class="box-form">
-		<marquee><h3>Formulir Pendaftaran</h3></marquee><hr><br>
+		<marquee><h3>data</h3></marquee><hr><br>
+		<CENTER><img src="img/profil/CM.png" height="200" width="200"></CENTER>
 		<?php 
 		if(isset($_GET['err1'])){
 			echo '<div class="alert-error">Maaf, input nomor telepon harus angka</div>';
@@ -15,20 +16,9 @@
 		<form action="proses-daftar.php" method="post">
 			Nama Lengkap :<br>
 			<input type="text" name="nama" required/><br><br>
-			Tempat Lahir :<br>
-			<input type="text" name="tmp_lhr" required/><br><br>
-			Tanggal Lahir :<br>
-			<input type="date" name="tgl_lhr" required/><br><br>
-			Jenis Kelamin :<br>
-			<select name="jk">
-				<option value="Laki-laki">Laki-laki</option>
-				<option value="Perempuan">Perempuan</option>
-			</select><br><br>
 			No. Telepon :<br>
 			<input type="text" name="telp" required/><br><br>
 			Email :<br>
-			<input type="email" name="email" required/><br><br>
-			Password :<br>
 			<input type="password" name="pass" required/><br><br>
 			Alamat Lengkap :<br>
 			<textarea name="alamat" rows="5" cols="50"></textarea><br></br><br></br>
