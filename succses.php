ec4024c5bd0ba214f61f564419508d118cba3aa8
reffyferdiyatno <62872628+reffyferdiyatno@users.noreply.github.com>
2020-07-03 14:10:20 +0700

    Add files via upload

---
+++ succses.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title>SUCCSES</title>
+    <link rel="stylesheet" type="text/css" href="css1/style1.css">
+    <link rel="stylesheet" type="text/css" href="css1/style2.css">
+	</head>
+<body>
+	<div id="profile">
+	  <b id="welcome">BERHASIL</b>
+	</div>
+    </br>
+<a href="data-pendaftar.php" class="button button2">DATA </a>
+  </body>
+</html>
